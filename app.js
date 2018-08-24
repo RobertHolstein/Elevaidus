0d011726db38ef99ed802a912aff2348cc219259
RobertHolstein <robert.j.holstein@gmail.com>
2018-08-24 00:44:54 -0700

    added muliple sprite charactors on one page. Needs work.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = 3005;
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
@@ -25,6 +25,57 @@ app.get('/world2', (req, res) => {
 app.get('/world3', (req, res) => {
     res.sendFile(__dirname + '/public/world3.html');
 });
+
+const game = io.of('/game');
+var players = {};
+
+
+game.on('connection',function(socket){
+    console.log('A user connected...')
+    socket.on('newplayer', () => {
+        players[socket.id] = {
+            playerId: socket.id,
+            x: randomInt(100,400),
+            y: randomInt(100,400),
+            sprite: "charactor"
+        };
+
+        socket.emit('updatePlayers', players);
+        socket.broadcast.emit('newplayer', players[socket.id]);
+    });
+ 
+        // when a player moves, update the player data
+    socket.on('playerMovement', function (direction) {
+        switch(direction){
+            case "down":
+                players[socket.id].y += 0.5;
+                break;
+            case "up":
+                players[socket.id].y -= 0.5;
+                break;
+            case "left":
+                players[socket.id].x -= 0.5;
+            break;
+            case "right":
+                players[socket.id].x += 0.5;
+                break;
+        }
+        // emit a message to all players about the player that moved
+        socket.emit('playerMoved', players[socket.id]);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        // remove this player from our players object
+        delete players[socket.id];
+        // emit a message to all players to remove this player
+        io.emit('disconnect', socket.id);
+    });
+});
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
 
 // tech namespace
 const tech = io.of('/tech');
--- public/js/game.js
+++ public/js/game.js
@@ -11,18 +11,70 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var self;
 
 function preload ()
 {
-    this.load.image('map', 'assets/map.png')
-    this.load.spritesheet('charactor', 'assets/charactor.png', { frameWidth: 48, frameHeight: 48 })
+    this.load.image('tileset', 'assets/map/tilesheet.png')
+    this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+    this.load.spritesheet('charactor', 'assets/sprites/charactor.png', { frameWidth: 48, frameHeight: 48 })
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'map');
-    this.player = this.add.sprite(100,100,'charactor');
+    self = this;
+    // ***** socket.io *****
+    this.socket = io('/game');
+
+    // ***** END socket.io *****
+
+    // ***** MAP *****
+    this.map = this.add.tilemap('map');
+    var tileset = this.map.addTilesetImage('tilesheet', 'tileset');
+    var layer;
+    for(var i = 0; i < this.map.layers.length; i++) {
+        layer = this.map.createStaticLayer(i, tileset, 0, 0);
+    }
+    layer.inputEnabled = true;
+    // ***** END MAP *****
+
+    // ***** PLAYERS *****
+    this.otherPlayers = []; //Create emplty array to put players into
+    this.socket.on('updatePlayers', function (players) {
+        Object.keys(players).forEach(function (id) {
+          if (players[id].playerId === self.socket.id) {
+            addPlayer(self, players[id]);
+          } else {
+            addOtherPlayers(self, players[id]);
+          }
+        });
+    });
+
+      this.socket.on('newPlayer', function (playerInfo) {
+        addOtherPlayers(self, playerInfo);
+      });
+
+      this.socket.on('disconnect', function (playerId) {
+        self.otherPlayers.forEach(function (otherPlayer) {
+          if (playerId === otherPlayer.playerId) {
+            otherPlayer.destroy();
+          }
+        });
+      });
+      
+    this.socket.on('playerMoved', (playerInfo) => {
+        self.otherPlayers.forEach( (Player) => {
+            if (playerInfo.playerId === Player.playerId) {
+                Player.setPosition(playerInfo.x, playerInfo.y);
+            }
+        });
+        if(playerInfo.playerId === self.player.playerId){
+            self.player.setPosition(playerInfo.x, playerInfo.y);
+        }
+    });
+
     cursors = this.input.keyboard.createCursorKeys();
+
     this.anims.create({
         key: 'walkUp',
         frames: this.anims.generateFrameNames('charactor', { frames: [ 2, 6, 10, 14 ] }),
@@ -47,29 +99,48 @@ function create ()
         frameRate: 3,
         repeat: 0
     });
-    
+    // ***** PLAYERS *****
+
+    this.socket.emit('newplayer');
+    console.log('new player created');
 }
 
 function update ()
 {
     if (cursors.left.isDown) // if the left arrow key is down
     {
-        this.player.x -= .5; // move left
+        PlayerMovement('left')
         this.player.anims.play('walkLeft', false);
     }
     if (cursors.right.isDown) // if the right arrow key is down
     {
-        this.player.x += .5; // move left
+        PlayerMovement('right');
         this.player.anims.play('walkRight', true);
     }
     if (cursors.up.isDown)
     {
-        this.player.y -= .5; // move left        
+        PlayerMovement('up');        
         this.player.anims.play('walkUp', true);
     }
     if (cursors.down.isDown)
     {
-        this.player.y += .5; // move left        
+        PlayerMovement('down');        
         this.player.anims.play('walkDown', true);
     }
+}
+
+
+function addPlayer(self, playerInfo) {
+  self.player = self.add.sprite(playerInfo.x,playerInfo.y, playerInfo.sprite);
+  self.player.playerId = playerInfo.playerId;
+}
+
+function addOtherPlayers(self, playerInfo) {
+  const otherPlayer = self.add.sprite(playerInfo.x,playerInfo.y, playerInfo.sprite);
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.push(otherPlayer);
+}
+
+function PlayerMovement(direction){
+    self.socket.emit('playerMovement', direction);
 }
